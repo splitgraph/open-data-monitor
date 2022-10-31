cca5065fc30a007caf8dec65e2073ca53c739008
Patrick Skinner <pas8314@gmail.com>
2022-10-31 17:58:46 +0000

    Introduce Dataset and DatasetList
    - Implement new styling per comp.
    - Wire is_added into the appropriate places to improve diff rendering
    - Switch from CSS modules to css-in-js to support dynamic 'badge'

--- components/Dataset.tsx
+++ components/Dataset.tsx
@@ -1,18 +1,62 @@
+import { useState } from 'react'
+import SplitgraphRepoUrl from './SplitgraphRepoUrl';
+import Button from './Button'
 import styles from '../styles/Dataset.module.css'
 
 interface DatasetProps {
-  name: string,
-  description: string,
+  id: string;
+  name: string;
+  domain: string;
+  description: string;
   isAdded: boolean;
 }
-const Dataset = ({ name, description, isAdded }: DatasetProps) =>
-  <div className={styles.dataset}
-    style={{
-      border: `1px solid ${isAdded ? 'green' : 'red'}`
-    }}>
-    <h6>{name}</h6>
-    <p>{description}</p>
-    {isAdded ? "added" : "deleted"}
-  </div >
+const Dataset = ({ id, name, domain, description, isAdded }: DatasetProps) => {
+  const [fullDesc, setFullDesc] = useState(false);
+  const [showRepo, setShowRepo] = useState(false);
+
+  return (
+    <div className={styles.dataset}>
+      <h4 style={{ width: '88%' }}>{name}</h4>
+      <div style={{
+        position: 'absolute',
+        top: '11px',
+        right: '-14px',
+        padding: '0.5rem',
+        width: '4rem',
+        background: isAdded ? '#009635' : 'red',
+        color: 'white',
+        textAlign: 'center',
+        boxShadow: `2px 2px 4px gray`,
+        borderRadius: '4px',
+        fontSize: '8pt'
+      }}>{isAdded ? "NEW" : "DELETED"}</div>
+      {description && <>
+        <p style={{
+          display: '-webkit-box',
+          fontSize: '12px',
+          ...(!fullDesc && {
+            overflow: 'hidden',
+            WebkitBoxOrient: 'vertical',
+            WebkitLineClamp: '2'
+          })
+
+        }}>
+          {description}
+        </p>
+      </>
+      }
+      <div className={styles.buttonFooter}>
+        {description && <Button onClick={() => setFullDesc(!fullDesc)}>{fullDesc ? 'less' : 'more'}</Button>}
+        <div style={{ width: '100%' }}>&nbsp;</div>
+        {/* If the repo is deleted, it doesn't make sense to link to a definitely missing page */}
+        {isAdded && <><Button onClick={() => { setShowRepo(!showRepo) }}>repo</Button> &nbsp;</>}
+        {showRepo && <div className={styles.repoButton}>
+          <SplitgraphRepoUrl id={id} name={name} domain={domain} />
+        </div>}
+      </div>
+    </div >
+  )
+}
+
 
 export default Dataset
--- components/DatasetList.tsx
+++ components/DatasetList.tsx
@@ -1,4 +1,5 @@
 import { useMemo } from 'react';
+import Link from 'next/link'
 import Dataset from './Dataset';
 import styles from '../styles/Dataset.module.css';
 
@@ -12,8 +13,9 @@ export interface DatasetType {
   is_added: boolean;
 }
 
+type DatasetNoDomain = Omit<DatasetType, "domain">
 interface RolledUpDatasets {
-  [domain: string]: Array<Omit<DatasetType, "domain">>;
+  [domain: string]: Array<DatasetNoDomain>;
 }
 
 const rollupData = (rows: Array<DatasetType>): RolledUpDatasets => {
@@ -42,17 +44,19 @@ const DatasetList = ({ data }: DatasetListProps) => {
         {
           Object.entries(rolledUp).map(([domain, datasets]) => {
             return (
-              <div key={domain}>
-
-                <h4><a id={`${domain}`}>{domain}</a></h4>
-                {
-                  datasets.map(({ name, description, is_added }) =>
-                    <Dataset key={name} name={name} description={description} isAdded={is_added} />
-                  )
-                }
-
-              </div>
-
+              <a id={`${domain}`} key={domain} >
+                <div className={styles.datasetAndDomain}>
+                  <h3>{domain}</h3>
+                  {
+                    datasets.map(({ id, name, description, is_added }) =>
+                      <Dataset key={name + id}
+                        id={id} name={name} domain={domain}
+                        description={description} isAdded={is_added}
+                      />
+                    )
+                  }
+                </div>
+              </a>
             )
           }
 
@@ -60,9 +64,48 @@ const DatasetList = ({ data }: DatasetListProps) => {
         }
       </div>
       <div className={styles.right}>
-        {Object.keys(rolledUp).map((domain) => <div key={domain}><a href={`#${domain}`}>{domain}</a></div>)}
+        <h4>Jump to...</h4>
+        {Object.entries(rolledUp).map(([domain, datasets]) =>
+          <div key={domain} className={styles.jumpToItem}>
+            <a href={`#${domain}`}>
+              {domain}
+            </a>&nbsp;
+            <AddsSubs addsSubs={getAddsSubs(datasets)} />
+          </div>
+        )}
+        <p className={styles.jumpToFooter}>
+          <Link href="/heatmap">Heatmap</Link>
+        </p>
       </div>
     </div>
   )
 }
 export default DatasetList
+
+const getAddsSubs = (datasets: Array<DatasetNoDomain>) => {
+  let adds = 0;
+  let subs = 0;
+  datasets.forEach((ds: DatasetNoDomain) => {
+    if (ds.is_added) { adds++ }
+    else { subs++ }
+  })
+
+  return { adds, subs };
+}
+
+const AddsSubs = ({ addsSubs }: { addsSubs: { adds: number, subs: number } }) => {
+  const { adds, subs } = addsSubs;
+  return (
+    <span>
+      {adds > 0 && <Adds adds={adds} />}
+      {((adds > 0) && (subs > 0)) && ", "}
+      {subs > 0 && <Subs subs={subs} />}
+    </span>
+  )
+}
+
+const Adds = ({ adds }: { adds: number }) =>
+  adds > 0 ? <span>{`+${adds}`}</span> : null
+
+const Subs = ({ subs }: { subs: number }) =>
+  subs > 0 ? <span>{`-${subs}`}</span> : null
