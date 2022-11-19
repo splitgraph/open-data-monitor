c55465193083cf4fc5741036a6509586a4a4e133
Patrick Skinner <pas8314@gmail.com>
2022-11-19 09:01:44 +0000

    Migrate /month to same approach as /week
    - Ensure SSR queries for 'month' things
    - Otherwise largely the same as /week page

--- pages/month/[month].tsx
+++ pages/month/[month].tsx
@@ -1,19 +1,51 @@
+import type { NextPage, GetServerSideProps } from 'next'
 import { useRouter } from 'next/router'
+import { SWRConfig } from 'swr'
+import { seafowlFetcher, latestKnownMonth, picker, monthlyDiff } from '../../data/seafowl'
 import RootLayout from '../../layouts/Root'
-import MonthlyDiff from '../../components/MonthlyDiff'
+import MonthlyDatasetList from '../../components/MonthlyDatasetList'
+import PickerContainer from '../../components/PickerContainer'
 
-const MonthPage = () => {
+const MonthPage: NextPage<{ fallback: any }> = ({ fallback }) => {
   const router = useRouter();
   const { month } = router.query;
 
   if (router.isFallback) {
     return <div>Loading...</div>
   }
+  const setTimestamp = (value: string) => {
+    const newQueryParams = { ...router.query, month: value };
+
+    router.replace({
+      pathname: router.pathname,
+      query: newQueryParams,
+    });
+  }
 
   return (
-    <RootLayout>
-      <MonthlyDiff timestamp={month as string} />
-    </RootLayout>
+    <SWRConfig value={{ fallback }}>
+      <RootLayout>
+        <PickerContainer timestamp={month as string} setTimestamp={setTimestamp} />
+        <MonthlyDatasetList timestamp={month as string} />
+      </RootLayout>
+    </SWRConfig>
   )
 }
+export const getServerSideProps: GetServerSideProps = async () => {
+  const latestKnownMonthRaw = await seafowlFetcher(latestKnownMonth);
+  const { latest: timestamp } = latestKnownMonthRaw.length && latestKnownMonthRaw[0]
+  const pickerResult = await seafowlFetcher(picker(timestamp))
+  const dailyDiffResult = await seafowlFetcher(monthlyDiff(timestamp))
+
+  return {
+    props: {
+      fallback: {
+        [latestKnownMonthRaw]: timestamp,
+        [picker(timestamp)]: pickerResult,
+        [monthlyDiff(timestamp)]: dailyDiffResult
+      }
+    }
+  }
+}
+
 export default MonthPage
