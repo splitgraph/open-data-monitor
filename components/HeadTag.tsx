febbe7001106f923296cf501bb2bdc305fc531eb
Patrick Skinner <pas8314@gmail.com>
2023-01-18 01:09:53 +0000

    Implement suggested changes
    - Clarify "open government data"
    - Apply new name scheme (part 1)

--- components/HeadTag.tsx
+++ components/HeadTag.tsx
@@ -2,8 +2,8 @@ import Head from 'next/head'
 
 const HeadTag = () =>
   <Head>
-    <title>SocFeed</title>
-    <meta name="description" content="A feed of Socrata diffs" />
+    <title>Open Data Monitor</title>
+    <meta name="description" content="A feed of open government data diffs" />
     <link rel="icon" href="/favicon.ico" />
   </Head>
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
           SocFeed
         </Link>
       </h2>
-      <p className={styles.description}>Track added and deleted datasets on Socrata government data portals</p>
+      <p className={styles.description}>Monitor added/deleted datasets on <a href="https://dev.socrata.com/">Socrata</a> open government data portals</p>
       <div className={styles.poweredBy}>
         Powered by <a href="https://www.splitgraph.com">Splitgraph</a>.
         <br /><br />
