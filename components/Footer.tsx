e8ca345f777fd1381691bd3d50ce79cf47fcedbe
Patrick Skinner <pas8314@gmail.com>
2023-01-20 19:26:14 +0000

    Move Seafowl attribution to header

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
       Feeling lucky? <a href="https://splitgraph.github.io/socrata-roulette/">Socrata Roulette</a><br />
       <br />
       <Image src="/brandmark.svg" alt="Splitgraph logo" width="35" height="35" /><br />
-      Powered by <a href="https://www.splitgraph.com" target="_new">Splitgraph</a>, Built with <a href="https://www.seafowl.io" target="_new">Seafowl</a>
+      Powered by <a href="https://www.splitgraph.com">Splitgraph</a>
       <div>
         Questions? Tweet us <a href="https://twitter.com/intent/tweet?text=@splitgraph">@splitgraph</a>
       </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
       </h2>
       <p className={styles.description}>Monitor added/deleted datasets on <a href="https://dev.socrata.com/">Socrata</a> open government data portals</p>
       <div className={styles.poweredBy}>
-        Powered by <a href="https://www.splitgraph.com">Splitgraph</a>
+        Powered by <a href="https://www.splitgraph.com" target="_new">Splitgraph</a>, Built with <a href="https://www.seafowl.io" target="_new">Seafowl</a>
         <br /><br />
       </div>
     </header>
