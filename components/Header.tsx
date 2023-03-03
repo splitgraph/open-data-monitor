b5ff8f6b0fe3b9fc61c26a06634298c8de45a2c2
Patrick Skinner <pas8314@gmail.com>
2023-03-03 02:55:40 +0000

    Add callout for blog post to index page

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,13 +3,18 @@ import styles from '../layouts/Root.module.css'
 
 const Header = () => {
   return (
-    <header>
+    <header className={styles.header}>
       <h2 className={styles.title}>
         <Link href="/">
           Open Data Monitor
         </Link>
       </h2>
       <p className={styles.description}>Monitor added/deleted datasets on <a href="https://dev.socrata.com/">Socrata</a> open government data portals</p>
+      <div className={styles.alert}>
+        <p>
+        ℹ️&nbsp;<a href="https://www.splitgraph.com/blog/opendatamonitor">See how</a>
+        </p>
+        </div>
       <div className={styles.poweredBy}>
         Powered by <a href="https://www.splitgraph.com" target="_new">Splitgraph</a>, Built with <a href="https://www.seafowl.io" target="_new">Seafowl</a>
         <br /><br />
